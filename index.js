825dd82534a696cf47f9bea723a9c44a306c302f
stremlenye <stremlenye@gmail.com>
2015-05-09 19:55:57 +0200

    Install some dev stuff

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+var keys = {
+  headers: 'headers'
+};
+
+app.set('port', (process.env.PORT || 5000));
+app.set(keys.headers, []);
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', function (req, res) {
-  console.log(req.headers);
-  res.send('Hello World!');
+  var headers = app.get(keys.headers);
+  headers.push(req.headers);
+  app.set(keys.headers, headers);
+  res.send(headers);
 });
 
-var server = app.listen(3000, "10.1.227.96", function () {
+var server = app.listen(app.get('port'), function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
